5ba1fc45d65a6867ff8d876fe95d892ba050a0f8
LuisMiguel999 <98474302+LuisMiguel999@users.noreply.github.com>
2023-03-10 07:08:50 -0500

    Add files via upload

---
+++ practica_operadores/Entradas de usuario.py
@@ -0,0 +1,12 @@
+color = input("Introduce el color de tu camisa?: ")
+print(color)
+# Introduce el color de tu camisa?: AZUL
+#AZUL
+n = int(input("Cuantas camisas tienes?: "))
+print(n)
+#Cuantas camisas tienes?: 5
+#5
+price = float(input("Cuanto costo la mas bonita?: "))
+print(price)
+#Cuanto costo la mas bonita?: 1000
+#1000.0
---
+++ practica_operadores/python_basics/1.py
@@ -0,0 +1,9 @@
+'''Escribir una función que reciba el nombre del usuario y un número entero e imprima por
+pantalla en líneas distintas el nombre del usuario tantas veces como el número
+introducido.
+'''
+nombre = input("¿Cuales tu nombre?")
+A = input ("Escribe un numero entero")
+print ((nombre + "\n") * int (A)) 
+    
+
